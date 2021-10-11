f1a85ae7095fffed52011fab18dca78baf4b1db2
ShestakovPavel <shestakov.exceedteam@gmail.com>
2021-10-11 14:30:04 +0300

    Задача по строкам -2

---
+++ newjs.js
@@ -0,0 +1,16 @@
+/*Дана строка:
+Я изучаю JavaScript
+Как вывести 3-й символ? 1-й? Последний?
+
+Приветствуется решение несколькими способами
+
+Решение должно быть залито в ваш репозиторий на github*/
+
+let str = "Я изучаю JavaScript"
+strLenght = str.length
+
+
+console.log(str[2])
+console.log(str[0])
+console.log(str.slice(-1))
+console.log(str[strLenght - 1])
